6a8e219861b6aa26bb7f908f4e2a399202bcef92
mukeshcs2018 <mukesh.cs2018@gmail.com>
2023-09-27 15:32:19 +0530

    add secret project using public api

--- 5.6 Secrets Project/index.js
+++ 5.6 Secrets Project/index.js
@@ -11,3 +11,28 @@
 // secret and the username of the secret.
 
 // 6. Listen on your predefined port and start the server.
+
+import axios from "axios";
+import express from "express";
+
+const app = express();
+const port = 3000;
+
+app.use(express.static("public"));
+
+app.get("/", async (req, res) => {
+  try {
+    const result = await axios.get("https://secrets-api.appbrewery.com/random");
+    res.render("index.ejs", {
+      secret: result.data.secret,
+      user: result.data.username,
+    });
+  } catch (error) {
+    console.log(error.response.data);
+    res.status(500);
+  }
+});
+
+app.listen(port, () => {
+  console.log("App is running on port " + port);
+});
